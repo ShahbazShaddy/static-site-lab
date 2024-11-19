35f429c89db6fb1ab4c4ed9e186fddafb02f986c
ShahbazShaddy <shahbazghafil@gmail.com>
2024-11-19 14:30:47 +0500

    Update static site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/static-site-lab',
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,14 @@
-import matter from 'gray-matter';
-import path from 'path';
-import fs from 'fs';
-
-export default function Home({ postData }) {
-    const { title, date, content } = postData;
-
-    return (
-        <div>
-            <h1>{title}</h1>
-            <p>{date}</p>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
-        </div>
-    );
-}
-
-// Fetch data at build time
-export async function getStaticProps() {
-    const postsDirectory = path.join(process.cwd(), 'posts');
-    const filePath = path.join(postsDirectory, 'hello-world.md');
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
-
-    return {
-        props: {
-            postData: { ...data, content },
-        },
-    };
+import Head from 'next/head';
+export default function Home() {
+ return (
+    <>
+        <Head>
+            <title>My Static Site</title>
+            <meta name="description" content="This is a sample static site." />
+            <meta property="og:title" content="My Static Site" />
+            <meta property="og:description" content="This is a sample static site using Next.js." />
+        </Head>
+        <h1>Welcome to My Static Site</h1>
+    </>
+ );
 }
