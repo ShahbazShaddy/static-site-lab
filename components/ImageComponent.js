1eb8424a84925be6c309bfcd4924f18513f59415
ShahbazShaddy <shahbazghafil@gmail.com>
2024-11-25 21:41:17 +0500

    Update static site build and manifest files for new deployment and remove unused components

--- components/ImageComponent.js
+++
@@ -1,8 +0,0 @@
-
-import Image from 'next/image';
-
-const ImageComponent = ({ src, alt, width, height }) => (
-  <Image src={src} alt={alt} width={width} height={height} />
-);
-
-export default ImageComponent;
